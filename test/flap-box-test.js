f7e060a3c7b06b92c09f0631bbe1f6d1b6c0a8da
xuexin <tomyail1989@gmail.com>
2016-10-04 21:33:38 +0800

    update test

--- test/flap-box-test.js
+++ test/flap-box-test.js
@@ -1,9 +1,10 @@
 import test from 'ava';
 import {mount} from 'enzyme';
 import FlapBox from  '../demo/examples/flap-box.jsx';
-import React,{ Component, PropTypes }  from 'react'
+import React, {Component, PropTypes}  from 'react'
 
 test(t=> {
-    const wrapper = mount(<FlapBox />)
-    t.true(wrapper.displayName)
+    const props = {key1: 'value1'};
+    const wrapper = mount(<FlapBox {...props}/>)
+    t.true(wrapper.props().key1 == props.key1);
 });
